0d06120926447bdcfa2496f53e02d7bfb61e324f
fenghou <johnhu456@163.com>
2016-05-05 14:19:49 +0800

    1、更改Cell样式 2、增加右导航栏按钮功能 3、需要构思更好的交互界面

---
+++ DevelopToolBox/DevelopToolBox/FontDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  FontDetailViewController.h
+//  DevelopToolBox
+//
+//  Created by MADAO on 16/5/5.
+//  Copyright © 2016年 MADAO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FontDetailViewController : UIViewController
+
+@end
--- DevelopToolBox/DevelopToolBox/FontViewController.m
+++ DevelopToolBox/DevelopToolBox/FontViewController.m
@@ -9,6 +9,8 @@
 #import "FontViewController.h"
 #import "FHTool.h"
 
+static CGFloat const cellHeight = 60.f;
+
 @interface FontViewController ()<UITableViewDelegate,UITableViewDataSource>
 
 #warning may need to change
@@ -23,17 +25,29 @@
 
 @property (nonatomic, strong) UITableView *mainFontTableView;
 
+/**示例文字*/
+@property (nonatomic, strong) NSString *sampleText;
 
 @end
 
 @implementation FontViewController
 
+- (void)setSampleText:(NSString *)sampleText
+{
+    _sampleText = sampleText;
+    if (self.mainFontTableView) {
+        [self.mainFontTableView reloadData];
+    }
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"All Font";
+    self.sampleText = @"SampleText";
     [self setupMainTableView];
     [self getAscendFontArray];
     [self getFirstCharacterArray];
+    [self setupRightNavigationBarButton];
 }
 
 - (void)setupMainTableView
@@ -78,6 +92,12 @@
     self.titleToIndexDictionary = mutaTitleToIndexDictionary;
 
 }
+
+- (void)setupRightNavigationBarButton
+{
+    UIBarButtonItem *rightBarItem = [[UIBarButtonItem alloc] initWithTitle:@"更改文字" style:UIBarButtonItemStyleDone target:self action:@selector(handleChangeTextButtonOnClicked)];
+    self.navigationItem.rightBarButtonItem = rightBarItem;
+}
 #pragma mark - UITableViewDataSourceAndDelegate
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
@@ -95,12 +115,18 @@
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"mainCell"];
     if (cell == nil){
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"mainCell"];
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"mainCell"];
     }
     NSArray *aFamily = self.fontArray[indexPath.section];
-    cell.textLabel.text = aFamily[indexPath.row];
+    cell.textLabel.text = self.sampleText;
+    cell.detailTextLabel.text = aFamily[indexPath.row];
     cell.textLabel.font = [UIFont fontWithName:aFamily[indexPath.row] size:20];
     return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return cellHeight;
 }
 
 - (NSArray *)sectionIndexTitlesForTableView:(UITableView *)tableView
@@ -116,14 +142,33 @@
     return [[self.titleToIndexDictionary objectForKey:title] integerValue];
     
 }
-//
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
 
 {
-    return [NSString stringWithFormat:@"%ld",(long)section];
-//    NSArray *arrayOfCharacters = [self sectionIndexTitlesForTableView:self.mainFontTableView];
-//    return [self.characterArray objectAtIndex:section];
+    return self.fontFamilyArray[section];
     
+}
+
+#pragma mark - ACtions
+- (void)handleChangeTextButtonOnClicked
+{
+    @WEAKSELF;
+    UIAlertController *changeTextAlertController = [UIAlertController alertControllerWithTitle:@"更改示例文字" message:@"请输入您想要更改的示例文字" preferredStyle:UIAlertControllerStyleAlert];
+
+    __block UITextField *sampleTextField = [[UITextField alloc] init];
+    [changeTextAlertController addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
+        sampleTextField = textField;
+    }];
+    UIAlertAction *confirmAction = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        weakSelf.sampleText = sampleTextField.text;
+    }];
+    
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        
+    }];
+    [changeTextAlertController addAction:confirmAction];
+    [changeTextAlertController addAction:cancelAction];
+    [self presentViewController:changeTextAlertController animated:YES completion:nil];
 }
 /*
 #pragma mark - Navigation
